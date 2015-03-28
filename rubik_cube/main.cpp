6824230f8a33354d5d3e7b02fd17745813d8a1c6
Franktian <tianyawen201209@hotmail.com>
2015-03-28 18:10:14 -0400

    Removed animation mode

--- rubik_cube/main.cpp
+++ rubik_cube/main.cpp
@@ -850,35 +850,6 @@ void display(void)
 	// Specify camera transformation
 	glTranslatef(camXPos, camYPos, camZPos);
 
-
-	// Get the time for the current animation step, if necessary
-	if( animate_mode )
-	{
-		float curTime = animationTimer->elapsed();
-
-		if( curTime >= keyframes[maxValidKeyframe].getTime() )
-		{
-			// Restart the animation
-			animationTimer->reset();
-			curTime = animationTimer->elapsed();
-		}
-
-		///////////////////////////////////////////////////////////
-		// README:
-		//   This statement loads the interpolated joint DOF vector
-		//   into the global 'joint_ui_data' variable. Use the
-		//   'joint_ui_data' variable below in your model code to
-		//   drive the model for animation.
-		///////////////////////////////////////////////////////////
-		// Get the interpolated joint DOFs
-		joint_ui_data->setDOFVector( getInterpolatedJointDOFS(curTime) );
-
-		// Update user interface
-		joint_ui_data->setTime(curTime);
-		glui_keyframe->sync_live();
-	}
-
-
     ///////////////////////////////////////////////////////////
     // TODO:
 	//   Modify this function to draw the scene.
